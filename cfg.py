1fa5cd3b380adcc8d0a8ba02f328740bbf348909
Bill Lytton <neuro014@gmail.com>
2019-10-11 11:24:07 -0400

    not sure what all this stuff is in the simconfig

--- cfg.py
+++ cfg.py
@@ -1,42 +1,18 @@
-#------------------------------------------------------------------------------
-# SIMULATION CONFIGURATION
-#------------------------------------------------------------------------------
-
 from netpyne import specs
+simConfig = specs.SimConfig()
 
-simConfig = specs.SimConfig()   # object of class SimConfig to store the simulation configuration
-
-celsius = 36
-v_init = -70
-
-# Simulation parameters
 simConfig.trans = 0000
-simConfig.Dt = 0.1# / 4
+simConfig.dt = 0.1 # / 4
 simConfig.steps_per_ms = 1/simConfig.Dt
 simConfig.npoints = 6000# * 4
-
-simConfig.duration = simConfig.npoints * simConfig.Dt # simConfig.trans + simConfig.npoints * simConfig.Dt # Duration of the simulation, in ms
-simConfig.dt = simConfig.Dt # Internal integration timestep to use
-simConfig.hParams['celsius'] = celsius
-simConfig.hParams['v_init'] = v_init
-simConfig.verbose = False 
+simConfig.duration = 400
+simConfig.hParams['celsius'], simConfig.hParams['v_init']  = 36, -70
 
 # Recording 
 simConfig.recordCells = [x for x in range(0, 40, 5)]
-
 simConfig.recordTraces = {'V_soma':{'sec':'soma','loc':0.5,'var':'v'}}
 simConfig.recordStim = True  # record spikes of cell stims
-simConfig.recordStep = 0.1 # Step size in ms to save data (eg. V traces, LFP, etc)
-
-
-# Saving
-simConfig.simLabel = "sim"
-simConfig.saveFolder = "data"
-simConfig.saveFileStep = 1000 # step size in ms to save data to disk
-simConfig.savePickle = True # Whether or not to write spikes etc. to a .mat file
-
-# netParams
-
-
+simConfig.recordStep = simConfig.dt 
+simConfig.saveFolder, simConfig.simLabel = "data", "sim"
 simConfig.runStopAt = simConfig.duration
 
